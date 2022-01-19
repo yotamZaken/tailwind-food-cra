ed88168c2b53390640dc44b272c0c342f0530454
yotamZaken <yotamzk@gmail.com>
2022-01-19 15:36:27 +0200

    Refactored HeaderCartButton component styling to TailwindCSS - older  styling left commented out for comparison

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -8,7 +8,7 @@ const HeaderCartButton = props => {
     const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const { items } = cartCtx;
+    const {items} = cartCtx;
 
     const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount
@@ -28,17 +28,22 @@ const HeaderCartButton = props => {
         }, 300);
 
         return () => {
-          clearTimeout(timerId)
+            clearTimeout(timerId)
         };
     }, [items]);
 
-    return <button className={btnClasses} onClick={props.onClick}>
-      <span className={classes.icon}>
-          <CartIcon/>
-      </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>{numberOfCartItems}</span>
-    </button>
+    return (
+        // <button className={btnClasses} onClick={props.onClick}>
+        <button className={`cursor-pointer border-none bg-slate-800 text-white py-3 px-12 flex justify-around items-center rounded-3xl font-bold hover:bg-black active:bg-black ${btnIsHighlighted ? 'animate-ping' : ''}`} onClick={props.onClick}>
+            {/*<span className={classes.icon}>*/}
+            <span className={'w-5 h-5 mr-2'}>
+                <CartIcon/>
+            </span>
+            <span>Your Cart</span>
+            {/*<span className={classes.badge}>{numberOfCartItems}</span>*/}
+            <span className={'bg-amber-700 py-1 px-4 rounded-3xl ml-4 font-bold'}>{numberOfCartItems}</span>
+        </button>
+    )
 };
 
 export default HeaderCartButton;
